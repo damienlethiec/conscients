89aa6d547163667a631f359c4b8adf2bc244fcf7
Hadrien Matringe <hadrien.matringe@gmail.com>
2019-09-20 22:39:35 +0200

    Fix problem routes static pages

--- config/routes.rb
+++ config/routes.rb
@@ -461,8 +461,8 @@ Rails.application.routes.draw do
       '/', status: 301
     )
     resources :sitemap_tests, only: :index
+    match '*path', to: 'pages#home', constraints: lambda { |req|
+      req.path.exclude? 'rails/active_storage'
+    }, via: %i[post put patch delete]
   end
-  match '*path', to: 'pages#home', constraints: lambda { |req|
-    req.path.exclude? 'rails/active_storage'
-  }, via: %i[get post put patch delete]
 end
