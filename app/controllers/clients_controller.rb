4a756f06013cb6b957461f77c144d10bdeda3c3a
Hadrien Matringe <hadrien.matringe@gmail.com>
2019-10-28 16:13:33 +0100

    Add producer_presentation and project_presentation to tree_plantation and display on /clients dashboard

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,6 +8,8 @@ class ClientsController < ApplicationController
     @orders = current_client.orders.finished.order_by_date.includes \
       :delivery_address, :line_items, :invoice_attachment
     @line_items = current_client.line_items.certificated.sort_by(&:created_at).reverse
+
+    documentation
     stats
   end
 
@@ -16,5 +18,21 @@ class ClientsController < ApplicationController
   def stats
     @quantity_of_trees_planted = current_client.quantity_of_trees_planted
     @tonne_co2_captured = current_client.tonne_co2_captured
+  end
+
+  def documentation
+    @docs = []
+    @line_items.map(&:tree_plantation)&.compact&.map do |tp|
+
+      @docs << { tp_id: tp.id, type: :producer_presentation,
+        filename: tp.producer_presentation.blob.filename.to_s
+      } if tp.producer_presentation.attached?
+
+      @docs << { tp_id: tp.id, type: :project_presentation,
+        filename: tp.project_presentation.blob.filename.to_s
+      } if tp.project_presentation.attached?
+
+    end
+    @docs.uniq! { |doc| doc[:filename] }
   end
 end
