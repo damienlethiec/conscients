7271c47a9b54e8b4df0e5bf5be2ab18a99921982
Hadrien Matringe <hadrien.matringe@gmail.com>
2020-07-18 09:48:15 +0200

    Provide wkhtmltopdf-heroku binary path in production

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -16,6 +16,8 @@ WickedPdf.config = {
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: Gem.bin_path('wkhtmltopdf-heroku', 'wkhtmltopdf-linux-amd64')
+
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
