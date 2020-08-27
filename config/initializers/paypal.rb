134c3fd22e27c1a60e7e3451d282118eb77be322
Hadrien Matringe <hadrien.matringe@gmail.com>
2020-08-27 11:26:10 +0200

    Fix paypal ssl certificate fail by using server s ca file

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -3,6 +3,7 @@
 PayPal::SDK.configure(
   mode: Rails.env.production? ? 'live' : 'sandbox',
   client_id: Rails.application.credentials.dig(Rails.env.to_sym, :paypal_client_id),
-  client_secret: Rails.application.credentials.dig(Rails.env.to_sym, :paypal_secret)
+  client_secret: Rails.application.credentials.dig(Rails.env.to_sym, :paypal_secret),
+  ssl_options: { ca_file: nil }
 )
 PayPal::SDK.logger = Rails.logger
